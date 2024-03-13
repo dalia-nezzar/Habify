f24aa1661e17b7422bdd97e7af27d076de03e5fa
Mame Diarra, Dalia <nezzar.dalia@gmail.com>
2024-03-13 10:23:44 +0100

    Initial Commit

---
+++ Habify/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Habify
+//
+//  Created by nezzar dalia on 13/03/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Habify/HabifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabifyApp.swift
+//  Habify
+//
+//  Created by nezzar dalia on 13/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct HabifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
